b068b07d63b76bf6688e9ea3de0eade3616999cc
Maksim Koroteev <maksimkoroteev@ttk.ru>
2025-08-31 17:09:09 +0300

    yet another commit

---
+++ GoTop.svelte
@@ -0,0 +1,34 @@
+<script>
+    import {onMount} from 'svelte';
+    import {Icons} from '/icons.js';
+
+    let scrolled;
+
+    onMount(() => {
+        window.onscroll = function() {
+            scrolled = document.documentElement.scrollTop;
+        }
+    })
+
+    function scroll(e) {
+        window.scrollTo({top: 0, behavior: 'smooth'})
+    }
+</script>
+
+<button class="BUTTON _ROUND _HUGE" class:scrolled on:click|preventDefault={scroll} title="Наверх">
+    {@html Icons.chevron_up({size:36})}
+</button>
+
+<style>
+    button {
+        display:flex;
+        visibility:hidden;
+        position:fixed;
+        right:24px;
+        bottom:24px;
+        /*transform:translateX(-30px);*/
+    }
+    .scrolled {
+        visibility:visible;
+    }
+</style>
---
+++ index.js
@@ -0,0 +1,7 @@
+export { storable } from '/les/storable.js';
+export { dropdownsStore } from '/les/dropdowns.js';
+export { Toaster } from '/les/toaster/toaster.js';
+export { default as Nprogress } from '/les/Nprogress.svelte';
+export { default as Loader } from '/les/Loader.svelte';
+export { default as Idk } from '/les/Idk.svelte';
+export { default as GoTop } from '/les/GoTop.svelte';
