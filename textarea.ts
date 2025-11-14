3cced27eb57ad79a4d6ca2c0d85e2df1cd25d37f
Maksim Koroteev <maksimkoroteev@ttk.ru>
2025-11-14 16:04:36 +0300

    YAC

--- textarea.ts
+++ textarea.ts
@@ -1,5 +1,8 @@
-export function Textarea(node) {
-    function autoresize() {
+import {tick} from 'svelte';
+
+export async function Textarea(node) {
+    async function autoresize() {
+        await tick();
         node.style.height = 'auto'; // Reset height to recalculate
         node.style.height = node.scrollHeight + 'px';
     }
